f7c1899b23c2d9bc45c71a18f3678b5d59780d63
pradeepta3456 <pratimahub323@gmail.com>
2024-11-26 13:32:21 +0545

    initial commit

---
+++ models/student.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace repositry_project1.models
+{
+    public class Student
+    {
+        0 references
+        public int ID {  get; set; }
+        0 references 
+         public string FirstName { get; set; }
+        0 references 
+        public string LastName { get; set; }
+        0 references 
+        public String Email { get; set; }
+        0 references 
+         public string Phone { get; set; }
+        0 references 
+        public DateTime EnrollmentDate { get; set; }
+        0 references
+        public string Address { get; set; }
+        0 references 
+        public DateTime CreatedDate { get; set; }
+        0 references 
+        public string CreatedBy { get; set; }
+
+    }
+}
